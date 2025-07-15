424b9695d99d7a69bbbd65d3ce2315a47adec148
seanwestley <swestley@kellyparkcapital.com>
2025-07-15 17:00:59 +0530

    Create tactical.js

---
+++ netlify/functions/tactical.js
@@ -0,0 +1,59 @@
+// netlify/functions/fetchsonora.js
+const docusign = require('docusign-esign');
+
+exports.handler = async function (event, context) {
+  try {
+    const integratorKey = process.env.DS_CLIENT_ID;
+    const userId = process.env.DS_USER_ID;
+    const authServer = 'account.docusign.com';
+    const privateKey = process.env.DS_PRIVATE_KEY.replace(/\\n/g, '\n');
+
+    const dsApi = new docusign.ApiClient();
+    dsApi.setOAuthBasePath(authServer);
+
+    const results = await dsApi.requestJWTUserToken(
+      integratorKey,
+      userId,
+      ['signature', 'impersonation'],
+      privateKey,
+      3600
+    );
+
+    const accessToken = results.body.access_token;
+
+    const userInfo = await dsApi.getUserInfo(accessToken);
+    const accountId = userInfo.accounts[0].accountId;
+    const basePath = userInfo.accounts[0].baseUri + '/restapi';
+
+    dsApi.setBasePath(basePath);
+    dsApi.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
+
+    const envelopesApi = new docusign.EnvelopesApi(dsApi);
+  const response = await envelopesApi.listStatusChanges(accountId, {
+  fromDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 100).toISOString(),
+});
+
+
+    const sonoraEnvelopes = (response.envelopes || []).filter(envelope =>
+      envelope.emailSubject?.toLowerCase().includes('Tactical')
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify(sonoraEnvelopes),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify({ message: error.message, stack: error.stack }),
+    };
+  }
+};
